39da87ebe91f2190e78c2cd6434fc75d88d86834
Ernest Witkowski <ewit6a@interia.pl>
2024-02-05 23:14:37 +0100

    Add models file

---
+++ models.py
@@ -0,0 +1,19 @@
+from app import db
+
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    authors = db.relationship('Author', secondary='book_author', backref=db.backref('books', lazy='dynamic'))
+    borrowed = db.relationship('Borrow', backref='book', lazy='dynamic')
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'title': self.title,
+            'authors': [author.name for author in self.authors],
+            'borrowed': [borrow.returned for borrow in self.borrowed]
+        }
+
+class Author(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
